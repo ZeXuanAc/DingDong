2dc0f35eccc243aa165fe1885e4c2ca35a663e3b
Xuan <chzexuan@163.com>
2019-04-25 15:12:01 +0800

    首页导航icon_foot更新，首页显示楼层

--- dingdong_flutter/android/app/src/main/java/com/zxac/dingdong_flutter/MapActivity.java
+++ dingdong_flutter/android/app/src/main/java/com/zxac/dingdong_flutter/MapActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
